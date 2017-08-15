f72c3a76d5e96e63ab43933fe64d5106eca47764
Wyrd00 <amy.tai0120@gmail.com>
2017-08-15 17:02:47 +0300

    populated all back-end files//added database relates #7 #12

---
+++ src/router.js
@@ -0,0 +1,20 @@
+const {handleHomeRoute, handlePublic, handleGetData, handleNewBook} = require('./handlers');
+
+const router = (request, response) => {
+	const endpoint = request.url.split('/')[1];
+
+	if (endpoint === '') {
+		handleHomeRoute(response);
+	} else if (endpoint.indexOf('public') === 0) {
+		handlePublic(response, request);
+	} else if (endpoint === 'books') {
+		handleGetData(response);
+	} else if (endpoint === 'add-new-book') {
+		handleNewBook(response, request)
+ 	} else {
+		response.writeHead(404, 'Content-Type: text/html');
+		response.end('<h1>404 file not found</h1>');
+	}
+};
+
+module.exports = router;
