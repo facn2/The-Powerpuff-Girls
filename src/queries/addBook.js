e5bec4fab16656ca148add091c731d75a43d821d
Wyrd00 <amy.tai0120@gmail.com>
2017-08-17 13:59:40 +0300

    fix pictures, fix addbookquery, and fix html and fix logout relates #52 #54 #48 #46 #60

--- src/queries/addBook.js
+++ src/queries/addBook.js
@@ -1,23 +1,24 @@
 const dbConnection = require ('../database/db_connection');
 
-const addNewBook = (book_name, author, user_id, cover_url, callback ) => {
+const addNewBook = (formInput, callback) => {
+	const { book_title, author, username, cover_url } = formInput;
 
-const addBookQuery = 'INSERT INTO books(book_name, author, user_id, cover_url) VALUES ($1, $2, $3, $4);';
+	const userInsertQuery = 'SELECT id FROM users WHERE users.username = $1;';
+	const userDetailsArray = [formInput.username];
 
-	// const { book_title : bookTitle , author, username, img_url : bookImgUrl } = formInput;
-
-	const addBookArray = [book_name, author, user_id, cover_url]
-
-	dbConnection.query(addBookQuery, addBookArray, (err) => {
+	dbConnection.query(userInsertQuery, userDetailsArray, (err, response) => {//response = id of username
 		if (err) {
-			console.log('error with adding book', err);
-			return
-		} else {
-			console.log('success adding book');
-			callback(null);
+			return callback(err)
 		}
-	})
+		
+		const bookInsertQuery = 'INSERT INTO books(book_name, author, user_id, cover_url) VALUES ($1, $2, $3, $4);';
+
+		const bookDetailsArray = [book_title, author, response.rows[0].id, cover_url];
+
+		dbConnection.query(bookInsertQuery, bookDetailsArray, (err) => {
+			callback(null)
+		})
+	});
 }
 
-
-module.exports = addNewBook
+module.exports = addNewBook;
--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,12 +1,13 @@
 const dbConnection = require('../database/db_connection.js');
 
 const getData = callback => {
-	const getQuery = 'SELECT books.book_name, books.author, books.cover_url, users.username, FROM books JOIN users ON books.user_id = users.id;';
-
+	const getQuery = 'SELECT books.book_name, books.author, books.cover_url, users.username FROM books JOIN users ON books.user_id = users.id;';
+	console.log(getQuery)
 	dbConnection.query(getQuery, (err, res) => {
 		if (err) {
 			return callback(err);
 		} else {
+			console.log('success grabbing data!')
 			callback(null, res.rows);
 		}
 	})
